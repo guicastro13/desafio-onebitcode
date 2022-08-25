a9bf557a89f18d661a1b2361483f7b02378e9624
Guilherme Castro <guii_1@hotmail.com>
2022-08-25 17:10:58 -0300

    feat:adicionado cors para consumo de api pelo front-end externamente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
-app.listen(3000);
+app.listen(3001, () => {
+    console.log("Sevidor online!")
+});
